e514978f051b433598ad45643f1f4ef4631ec680
debor4230908 <debora.rodrigues.araujo@escola.pr.gov.br>
2025-04-09 10:49:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,4 +24,7 @@ document.addEventListener('DOMContentLoaded', function(){
     })
  })
  
- 
+ ScrollReveal().reveal('#inicio', {delay: 500});
+ ScrollReveal().reveal('#tropicalia', {delay: 500});
+ ScrollReveal().reveal('#galeria', {delay: 500});
+ ScrollReveal().reveal('#contato', {delay: 500});
